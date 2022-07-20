991582b74cfaacbedc102a4a7199b8f0338d2575
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-20 18:34:23 +0430

    testing tailwind

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className="text-6xl">
+        <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
